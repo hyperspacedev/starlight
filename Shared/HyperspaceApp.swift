5e3a1f52128ed5712fa5129b9fb1b1d9da2b17af
Marquis Kurt <software@marquiskurt.net>
2020-06-23 20:33:26 -0400

    [labs-swiftui] Create multiplatform project

---
+++ Shared/HyperspaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyperspaceApp.swift
+//  Shared
+//
+//  Created by Marquis Kurt on 6/23/20.
+//
+
+import SwiftUI
+
+@main
+struct HyperspaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
