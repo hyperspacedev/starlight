b395440dadaf2802762ae00ec7320f69e40ca9e3
Marquis Kurt <software@marquiskurt.net>
2020-07-07 11:28:23 -0400

    [Shared/Models/Classes] Add Mention model

---
+++ Shared/Models/Classes/Mention.swift
@@ -0,0 +1,32 @@
+//
+//  Mention.swift
+//  Codename Starlight
+//
+//  Created by Marquis Kurt on 7/7/20.
+//
+
+import Foundation
+
+/**
+ A class representation of a mention.
+ 
+ A mention model contains the data for an account that is mentioned in a given post.
+ */
+public class Mention: Codable, Identifiable {
+    /// The ID associated with this mention from the server.
+    // swiftlint:disable:next identifier_name
+    public let id: String
+
+    /// The mentioned account's username.
+    public let username: String
+
+    /**
+     The mentioned account's given username and domain, if applicable.
+     
+     This will otherwise be equivalent to the `username` value for this account.
+     */
+    public let acct: String
+
+    /// The website URL to the mentioned account's profile.
+    public let url: String
+}
