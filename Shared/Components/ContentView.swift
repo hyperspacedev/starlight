ff0c0a42834bd6b15aec706046493e714e039d49
Marquis Kurt <software@marquiskurt.net>
2020-07-08 16:41:35 -0400

    [Shared] Add accent color, more traditional layout stuff

--- Shared/Components/ContentView.swift
+++ Shared/Components/ContentView.swift
@@ -7,12 +7,25 @@
 
 import SwiftUI
 
+/// The structured view for the app
 struct ContentView: View {
+
+    #if os(iOS)
+    /// Determines whether the device is compact or standard
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+    #endif
+
     var body: some View {
         Group {
             #if os(macOS)
             StandardNavigationLayout()
                 .frame(minWidth: 500, minHeight: 300)
+            #else
+            if horizontalSizeClass == .compact {
+
+            } else {
+                StandardNavigationLayout()
+            }
             #endif
         }
     }
--- Shared/Components/Navigation/Standard.swift
+++ Shared/Components/Navigation/Standard.swift
@@ -10,59 +10,151 @@ import SwiftUI
 /// The navigation layout on bigger devices such as iPads and Macs.
 struct StandardNavigationLayout: View {
 
+    /// An enumeration of the different timeline pages
+    enum NavigationViews {
+        case home
+        case local
+        case `public`
+        case messages
+        case announcements
+        case activity
+        case recommended
+        case searchResults
+        case notifications
+    }
+
     /// The search bar's current text input.
     @State private var searchText = ""
+
+    /// The current navigation selection
+    @State private var selection: Set<NavigationViews> = [.home]
 
     /// The primary view.
     var body: some View {
         HStack {
             NavigationView {
-                VStack(alignment: .leading) {
-
+                VStack {
                     #if os(macOS)
                     TextField("Search...", text: $searchText)
-                        .padding(.horizontal)
-                        .cornerRadius(4)
+                        .padding()
+                        .padding(.trailing, 8)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                     #endif
 
-                    List {
-                        NavigationLink(
-                            destination:
-                                Text("Home timeline")
+                    List(selection: $selection) {
+                        Section {
+                            NavigationLink(
+                                destination:
+                                    Text("Home timeline")
+                                        .padding()
+                                        .frame(maxWidth: .infinity, maxHeight: .infinity),
+                                label: {
+                                    Label("Home", systemImage: "house")
+                                })
+                                .accessibility(label: Text("Home"))
+                                .tag(NavigationViews.home)
+                            NavigationLink(
+                                destination:
+                                    Text("Local timeline")
+                                        .padding()
+                                        .frame(maxWidth: .infinity, maxHeight: .infinity),
+                                label: {
+                                    Label("Local", systemImage: "building.2")
+                                })
+                                .accessibility(label: Text("Local"))
+                                .tag(NavigationViews.local)
+                            NavigationLink(
+                                destination:
+                                    Text("Public timeline")
+                                        .padding()
+                                        .frame(maxWidth: .infinity, maxHeight: .infinity),
+                                label: {
+                                    Label("Public", systemImage: "globe")
+                                })
+                                .accessibility(label: Text("Public"))
+                                .tag(NavigationViews.public)
+                            NavigationLink(
+                                destination:
+                                    Text("Messages")
+                                        .padding()
+                                        .frame(maxWidth: .infinity, maxHeight: .infinity),
+                                label: {
+                                    Label("Messages", systemImage: "quote.bubble")
+                                })
+                                .accessibility(label: Text("Messages"))
+                                .tag(NavigationViews.messages)
+                            #if os(iOS)
+                            NavigationLink(
+                                destination:
+                                    Text("Search")
+                                        .padding()
+                                        .frame(maxWidth: .infinity, maxHeight: .infinity),
+                                label: {
+                                    Label("Search", systemImage: "magnifyingglass")
+                                })
+                                .accessibility(label: Text("Search"))
+                                .tag(NavigationViews.searchResults)
+                            #endif
+                        }
+
+                        Section {
+                            NavigationLink(
+                                destination:
+                                    Text("Announcements")
                                     .padding()
                                     .frame(maxWidth: .infinity, maxHeight: .infinity),
-                            label: {
-                                Label("Home", systemImage: "house")
-                            })
-                        NavigationLink(
-                            destination:
-                                Text("Local timeline")
+                                label: {
+                                    Label("Announcements", systemImage: "megaphone")
+                                })
+                                .accessibility(label: Text("Announcements"))
+                                .tag(NavigationViews.announcements)
+                            NavigationLink(
+                                destination:
+                                    Text("Activity")
                                     .padding()
                                     .frame(maxWidth: .infinity, maxHeight: .infinity),
-                            label: {
-                                Label("Local", systemImage: "building.2")
-                            })
-                        NavigationLink(
-                            destination:
-                                Text("Public timeline")
+                                label: {
+                                    Label("Activity", systemImage: "flame")
+                                })
+                                .accessibility(label: Text("Activity"))
+                                .tag(NavigationViews.activity)
+                            NavigationLink(
+                                destination:
+                                    Text("Recommended")
                                     .padding()
                                     .frame(maxWidth: .infinity, maxHeight: .infinity),
-                            label: {
-                                Label("Public", systemImage: "globe")
-                            })
+                                label: {
+                                    Label("Recommended", systemImage: "person.2")
+                                })
+                                .accessibility(label: Text("Recommended"))
+                                .tag(NavigationViews.recommended)
+                        }
                     }
                     .listStyle(SidebarListStyle())
-                    .frame(maxWidth: .infinity)
+                    .frame(minWidth: 170, maxWidth: .infinity, maxHeight: .infinity)
                 }
-                .frame(minWidth: 170, idealWidth: 180, maxWidth: .infinity)
+                .frame(minWidth: 170, idealWidth: 180, maxWidth: .infinity, maxHeight: .infinity)
             }
             .toolbar {
                 #if os(macOS)
                 ToolbarItem(placement: .navigation) {
                     Button(action: toggleSidebar) {
                         Image(systemName: "sidebar.left")
+                    }.help("Show or hide the sidebar.")
+                }
+
+                ToolbarItem {
+                    Button(action: {}) {
+                        Image(systemName: "bell")
                     }
+                    .help("View your notifications.")
+                    .tag(NavigationViews.notifications)
+                }
+
+                ToolbarItem {
+                    Button(action: {}) {
+                        Image(systemName: "square.and.pencil")
+                    }.help("Write a new post.")
                 }
                 #endif
             }
