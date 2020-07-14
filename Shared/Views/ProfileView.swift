04e1d4a4a5f8fb619d4b545f578a64b7fe988bbd
Marquis Kurt <software@marquiskurt.net>
2020-07-14 15:40:35 -0400

    [Shared/Views] Attempt to fix ProfileView?

--- Shared/Views/ProfileView.swift
+++ Shared/Views/ProfileView.swift
@@ -20,148 +20,159 @@ struct ProfileView: View {
     ]
 
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading) {
-
-                Image("sotogrande")
-                    .resizable()
-//                    .scaledToFit()
-                    .overlay(
-                        Image("pointFlash")
-                            .resizable()
-                            .clipShape(Circle())
-                            .frame(width: 100, height: 100)
-                            .padding()
-                            .background(
-                                Circle()
-                                    .frame(width: 110, height: 110)
-                                    .foregroundColor(.white)
-                            )
-                            .offset(y: 64),
-                        alignment: .bottomLeading
-                    )
-
+        NavigationView {
+            ScrollView {
                 VStack(alignment: .leading) {
+                    Image("sotogrande")
+                        .resizable()
+                        .overlay(
+                            ProfileImage(from: accountInfo.data?.avatarStatic,
+                                         placeholder: {
+                                            Circle()
+                                                .scaledToFit()
+                                                .frame(width: 100, height: 100)
+                                                .foregroundColor(.gray)
+                                         },
+                                         size: 100)
+                                .frame(width: 100, height: 100)
+                                .padding()
+                                .background(
+                                    Circle()
+                                        .frame(width: 110, height: 110)
+                                        .foregroundColor(.white)
+                                )
+                                .offset(y: 64),
+                            alignment: .bottomLeading
+                        )
 
-                    HStack {
-
-                        Text(accountInfo.data?.displayName ?? "E")
-                            .font(.title)
-                            .bold()
-
-                        if let username = accountInfo.data?.displayName {
-
-                            if devs.contains(username) {
-                                Text("STARLIGHT DEV")
-                                    .foregroundColor(.secondary)
-                                    .padding(.all, 5)
-                                    .font(.caption2)
-                                    .background(
-                                        Color(.systemGray5)
-                                            .cornerRadius(3)
-                                    )
-                            }
-
-                        }
-
-                        if let isBot = accountInfo.data?.bot {
-
-                            if isBot {
-                                Text("BOT")
-                                    .foregroundColor(.white)
-                                    .padding(.all, 5)
-                                    .font(.caption2)
-                                    .background(
-                                        Color.blue
-                                            .cornerRadius(3)
-                                    )
-                            }
-
-                        }
-
-                    }
-
-                    Text("@iamnotabug")
-                        .font(.callout)
-                        .foregroundColor(.secondary)
-
-                    Text("Photographer for @ScotiaNews.")
-                        .padding(.top, 10)
-                }
-                    .padding(.horizontal)
-                    .padding(.top, 40)
-
-                self.stats
-
-                VStack(alignment: .leading) {
-                    HStack(spacing: 16.0) {
-                        if editable {
-                            Button(action: {}, label: {
-                                Label("Edit", systemImage: "square.and.pencil")
-                            })
-                        } else {
-                            Button(action: {}, label: {
-                                Label("Follow", systemImage: "person.badge.plus")
-                            })
-                        }
-                        Button(action: {}, label: {
-                            Label("Mention", systemImage: "bubble.left")
-                        })
-                        Spacer()
-                        Button(action: {}, label: {
-                            Image(systemName: "ellipsis.circle")
-                        })
-                        .contextMenu {
-
-                            Button(action: {}, label: {
-                                Label("Share", systemImage: "square.and.arrow.up")
-                            })
-
-                            Button(action: {}, label: {
-                                Label("Show more info", systemImage: "tablecells")
-                            })
-
-                            if !editable {
-                                Button(action: {}, label: {
-                                    Label("Block @iamnotabug", systemImage: "person.crop.circle.fill.badge.xmark")
-                                })
-                                Button(action: {}, label: {
-                                    Label("Report @iamnotabug", systemImage: "hand.raised.fill")
-                                })
-                            }
-                        }
-                    }
-                }
-                    .padding(.horizontal)
-
-                List {
-                Section {
-                    if self.accountInfo.statuses.isEmpty {
+                    VStack(alignment: .leading) {
                         HStack {
-                            Spacer()
-                            VStack {
-                                Spacer()
-                                ProgressView(value: 0.5)
-                                    .progressViewStyle(CircularProgressViewStyle())
-                                Text("Loading posts...")
-                                Spacer()
+
+                            Text(accountInfo.data?.displayName ?? "User")
+                                .font(.title)
+                                .bold()
+
+                            if let username = accountInfo.data?.displayName {
+
+                                if devs.contains(username) {
+                                    Text("STARLIGHT DEV")
+                                        .foregroundColor(.secondary)
+                                        .padding(.all, 5)
+                                        .font(.caption2)
+                                        .background(
+                                            Color(.systemGray5)
+                                                .cornerRadius(3)
+                                        )
+                                }
+
                             }
-                            Spacer()
+
+                            if let isBot = accountInfo.data?.bot {
+
+                                if isBot {
+                                    Text("BOT")
+                                        .foregroundColor(.white)
+                                        .padding(.all, 5)
+                                        .font(.caption2)
+                                        .background(
+                                            Color.blue
+                                                .cornerRadius(3)
+                                        )
+                                }
+
+                            }
+
                         }
-                    } else {
-                        ForEach(self.accountInfo.statuses, id: \.self.id) { status in
-                            StatusView(status: status)
-                                .buttonStyle(PlainButtonStyle())
+
+                        Text("@\(accountInfo.data?.acct ?? "user")")
+                            .font(.callout)
+                            .foregroundColor(.secondary)
+
+                        Text("\(accountInfo.data?.note ?? "No bio provided.")")
+                            .padding(.top, 10)
+                            .fixedSize(horizontal: false, vertical: true)
+                    }
+                        .padding(.horizontal)
+                        .padding(.top, 40)
+
+                    self.stats
+
+                    VStack(alignment: .leading) {
+                        HStack(spacing: 16.0) {
+                            if editable {
+                                Button(action: {}, label: {
+                                    Label("Edit", systemImage: "square.and.pencil")
+                                })
+                            } else {
+                                Button(action: {}, label: {
+                                    Label("Follow", systemImage: "person.badge.plus")
+                                })
+                            }
+                            Button(action: {}, label: {
+                                Label("Mention", systemImage: "bubble.left")
+                            })
+                            Spacer()
+                            Button(action: {}, label: {
+                                Image(systemName: "ellipsis.circle")
+                            })
+                            .contextMenu {
+
+                                Button(action: {}, label: {
+                                    Label("Share", systemImage: "square.and.arrow.up")
+                                })
+
+                                Button(action: {}, label: {
+                                    Label("Show more info", systemImage: "tablecells")
+                                })
+
+                                if !editable {
+                                    Button(action: {}, label: {
+                                        Label("Block @\(accountInfo.data?.acct ?? "user")", systemImage: "person.crop.circle.fill.badge.xmark")
+                                    })
+                                    Button(action: {}, label: {
+                                        Label("Report @\(accountInfo.data?.acct ?? "user")", systemImage: "hand.raised.fill")
+                                    })
+                                }
+                            }
                         }
                     }
+                        .padding(.horizontal)
+
+                    List {
+                    Section {
+                        if self.accountInfo.statuses.isEmpty {
+                            HStack {
+                                Spacer()
+                                VStack {
+                                    Spacer()
+                                    ProgressView(value: 0.5)
+                                        .progressViewStyle(CircularProgressViewStyle())
+                                    Text("Loading posts...")
+                                    Spacer()
+                                }
+                                Spacer()
+                            }
+                        } else {
+                            ForEach(self.accountInfo.statuses, id: \.self.id) { status in
+                                StatusView(status: status)
+                                    .buttonStyle(PlainButtonStyle())
+                            }
+                        }
+                    }
+                    .listStyle(PlainListStyle())
                 }
-                .listStyle(PlainListStyle())
+            }
+                .edgesIgnoringSafeArea(.top)
+                .onAppear {
+                    self.accountInfo.fetchProfile()
+                    self.accountInfo.fetchProfileStatuses()
+                }
             }
         }
-            .edgesIgnoringSafeArea(.top)
-            .onAppear {
-                self.accountInfo.fetchProfile()
-            }
+        .navigationBarHidden(true)
+        .navigationBarTitle("")
+        .edgesIgnoringSafeArea(.top)
+        
     }
 
     var stats: some View {
