2ddfeaec032aac905eba02dd2f9e2b0208269755
Marquis Kurt <software@marquiskurt.net>
2020-09-26 14:15:51 -0400

    [Shared/Views] Add access token-based login UI

--- Shared/Views/LoginView.swift
+++ Shared/Views/LoginView.swift
@@ -10,48 +10,73 @@ import SwiftUI
 struct LoginView: View {
 
     @State var instanceField: String = ""
+    @State var accessTokenField: String = ""
 
     var body: some View {
-        VStack {
-            Spacer()
-            // TODO: Replace this image with the Starlight icon.
-            Image(systemName: "person.crop.circle.fill.badge.plus")
-                .resizable()
-                .scaledToFit()
-                .frame(maxWidth: 128)
-            VStack(spacing: 8) {
-            Text("Sign in to Starlight")
-                .font(.title)
-                .bold()
-            Text(
-                "Sign in with your fediverse account to view your personal feed,"
-                + " chat with friends, edit your profile, and make posts on the "
-                + "fediverse."
-            )
-            .font(.caption)
-            }
-            VStack(spacing: 8) {
-                HStack {
-                    TextField("mastodon.online", text: $instanceField)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                    // TODO: Hook this up to some help dialog to explain what the
-                    // instance domain is.
+        NavigationView {
+            VStack {
+                Spacer()
+                // TODO: Replace this image with the Starlight icon.
+                Image(systemName: "person.crop.circle.fill.badge.plus")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: 128)
+                VStack(spacing: 8) {
+                Text("Sign in to Starlight")
+                    .font(.title)
+                    .bold()
+                Text(
+                    "Sign in with your fediverse account to view your personal feed,"
+                    + " chat with friends, edit your profile, and make posts on the "
+                    + "fediverse."
+                )
+                .font(.caption)
+                }
+                VStack(spacing: 8) {
+                    HStack {
+                        TextField("mastodon.online", text: $instanceField)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        // TODO: Hook this up to some help dialog to explain what the
+                        // instance domain is.
+                        Button(action: {}) {
+                            Image(systemName: "questionmark.circle")
+                        }
+                    }
                     Button(action: {}) {
-                        Image(systemName: "questionmark.circle")
+                        Text("Sign in")
                     }
                 }
-                Button(action: {}) {
-                    Text("Sign in")
-                }
+                .padding()
+                Spacer()
+                NavigationLink("Got an access token?", destination: accessToken)
+                    .font(.footnote)
             }
             .padding()
+        }
+    }
+    
+    var accessToken: some View {
+        VStack {
+            VStack(alignment: .leading) {
+                Text(
+                    "If you have already made an access token for this app or cannot log in"
+                    + " normally, you can add it here. Starlight will take care of the rest."
+                )
+            }            
+            TextField("Access token", text: $accessTokenField)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button(action: {}) {
+                Text("Sign in with access token")
+            }
             Spacer()
             Button(action: {}) {
-                Text("Got an access token?")
+                Text("Create an access token")
                     .font(.footnote)
             }
         }
         .padding()
+        .navigationTitle("Use an access token")
     }
 }
 
